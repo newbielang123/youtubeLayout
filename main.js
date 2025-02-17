7fa9391eac347796f05fa53b519b1274797484e6
simplejvcode <jeevierebiasv.4@gmail.com>
2025-02-17 16:23:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function changeColor(){
+    const colorText = document.getElementById("h1");
+    colorText.innerText = "hi";
+    colorText.style.color = "red";
+    colorText.style.backgroundColor = "black";
+    colorText.style.padding = "90px";
+    document.body.style.backgroundColor = "yellow";
+}
+
+
+
+
+
+
+
+function inputText(){
+    const textBox = document.getElementById("myInput").value;
+    alert(textBox)
+}
+
+
+
+
+
+
+
+
